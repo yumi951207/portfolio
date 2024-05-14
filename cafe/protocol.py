d3d21fd9616d82624ba8828f047f50c5d4db862d
yumi951207 <tabbycat221016@gmail.com>
2024-05-14 19:16:25 +0900

    Protocolの実装

--- cafe/protocol.py
+++ cafe/protocol.py
@@ -1,10 +1,23 @@
 from typing import Protocol
 
 class GetUrlsProtocol(Protocol):
-    def access_site(self,url):
+    def access_site(self, url) -> str:
         ...
 
 
 class GetinfoProtocol(Protocol):
-    def get_info(self):
+    def get_info(self,shop_soup, name, na_wifi, address) -> str:
         ...
+
+class GetCSVProtocol(Protocol):
+    def list_to_csv(self,list, list_name) -> str:
+        ...
+
+def url_func(geturlsprotocol: GetUrlsProtocol):
+    geturlsprotocol.access_site()
+
+def info_func(getinfoprotocol: GetinfoProtocol):
+    getinfoprotocol.get_info()
+
+def info_func(getcsvprotocol: GetCSVProtocol):
+    getcsvprotocol.list_to_csv()
